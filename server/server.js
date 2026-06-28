40cc38895fb760d405eff266834be77bc3836c4e
Omkar2240 <omkarramgirwar123@gmail.com>
2026-06-28 19:42:36 +0530

    added

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const taskRoutes = require("./routes/tasks");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
